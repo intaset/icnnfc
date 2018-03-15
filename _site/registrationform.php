74abc21eb92c9ee95793f5065b15cb52e7049162
administrator@international-aset.com <ICNFAicepr1120>
2018-03-15 17:09:03 -0400

    fixed registration form links issue

--- _site/registrationform.php
+++ _site/registrationform.php
@@ -43,9 +43,9 @@ $message = 'Dear Colleague,
 
 Thank you for registering for ICNNFC 2018. If you have requested any official letters, please allow up to 5 business days to receive your documents.
 
-If you are an author, please make sure to send us the final version of your paper and a signed copyright form via email to info@icnnfc.com. You can find the copyright form here: www.icnnfc.com/papers. Please note that failing to do so may result in an unsuccessful process of your registration.
+If you are an author, please make sure to send us the final version of your paper and a signed copyright form via email to info@icnnfc.com. You can find the copyright form here: https://icnnfc.com/papers. Please note that failing to do so may result in an unsuccessful process of your registration.
 
-To reserve a room with a DISCOUNTED price, please fill out the provided booking form here: www.icnnfc.com/accommodation/#Novotel
+To reserve a room with a DISCOUNTED price, please fill out the provided booking form here: https://icnnfc.com/accommodation/#Novotel
 
 You can find your registration details below. If there are any errors in the information you have provided, please write an email to us at registration@icnnfc.com mentioning the correct information. Please note that you SHOULD NOT refill the form.
 
